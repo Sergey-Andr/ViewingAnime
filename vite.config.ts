07655318cedb61ce67b349d002c7404fb56e08aa
Sergey <qukviktv@gmail.com>
2024-03-04 07:32:33 +0200

    some fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export default defineConfig({
     plugins: [react(), svgr({ include: "**/*.svg?react" })],
-    server: {
-        proxy: {
-            "/myanimelist": {
-                target: `https://api.myanimelist.net/v2`,
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/myanimelist/, ""),
-            },
-        },
-    },
+    server: isProduction
+        ? undefined
+        : {
+              proxy: {
+                  "/myanimelist": {
+                      target: `https://api.myanimelist.net/v2`,
+                      changeOrigin: true,
+                      rewrite: (path) => path.replace(/^\/myanimelist/, ""),
+                  },
+              },
+          },
 });
